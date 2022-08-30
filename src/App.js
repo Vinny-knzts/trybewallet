19578b4ebc0f5db2b9fde36cba33902c8d6ef206
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-08-30 12:44:51 -0400

    Completa requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Header from './components/Header';
+import Wallet from './pages/Wallet';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <Switch>
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
         </Switch>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './Header.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <div className="headerContainer">
-        <p className="headerText">TrybeWallet</p>
+        <p className="headerTitle">TrybeWallet</p>
+        <div className="headerInfoContainer">
+          <div className="headerInfo">
+            <p>Email:</p>
+            <p data-testid="email-field" className="headerText">{email}</p>
+          </div>
+          <div className="headerInfo">
+            <p className="headerText">Dispesa Total:</p>
+            <p data-testid="total-field">0</p>
+            <p data-testid="header-currency-field" className="headerText">BRL</p>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
