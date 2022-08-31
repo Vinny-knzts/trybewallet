6a876f817490ed6b2d3c6c4f9ec0c6e9efb4774e
Vinicius Moraes <viniciusmasouza@hotmail.com>
2022-08-31 16:19:51 -0400

    Completa requisitos 6 e e 7.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import './Table.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
-      <div>Table</div>
+      <table className="tableConteiner">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{(parseFloat(expense.value)).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                {(parseFloat(expense.exchangeRates[expense
+                  .currency].ask)).toFixed(2)}
+              </td>
+              <td>
+                {(parseFloat(expense.value) * expense
+                  .exchangeRates[expense.currency].ask).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.shape({
+    map: PropTypes.func.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
